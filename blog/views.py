6f23e2fa2bd36a19196a3276376cade8d9daca4f
Anton <gagonin1996@mail.ru>
2021-12-21 06:18:42 +0800

    Final blog without deployment on Heroku

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -16,3 +17,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title','body']
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
